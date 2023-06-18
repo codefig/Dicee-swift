16192d39afb16bba0d6e24b6162d05b761d77742
Moshood Alaran <geekyshood@gmail.com>
2023-06-18 10:27:26 +0100

    roll dice works

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageviewTwo: UIImageView!
+    @IBOutlet weak var diceImageViewOne: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        diceImageViewOne.image = UIImage(imageLiteralResourceName: "DiceSix")
+        diceImageviewTwo.image = UIImage(imageLiteralResourceName: "DiceSix")
+        diceImageViewOne.alpha = 0.5
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func rollButton(_ sender: UIButton) {
+       
+        
+        let imageArray : [String] = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive", "DiceSix"];
+        let randomOne = Int.random(in: 0...5);
+        let randomTwo = Int.random(in: 0...5);
+        diceImageViewOne.image = UIImage(imageLiteralResourceName: imageArray[randomOne])
+        diceImageviewTwo.image = UIImage(imageLiteralResourceName: imageArray[randomTwo])
+        
+    }
 }
 
